f8ed9a033e7a27e2da0f696cbe9044d3f90b760b
julio <j38600@gmail.com>
2016-07-28 12:41:00 +0100

    edit update
    update do ficheiro do controller...

--- application/controllers/Militar.php
+++ application/controllers/Militar.php
@@ -78,11 +78,69 @@ class Militar extends CI_Controller {
             redirect('militar', 'refresh');
         }
         $info = array();
+        
+        $this->form_validation->set_rules('nim', 'Número de Indentificação Militar', 'trim|required');
+        $this->form_validation->set_rules('nome', 'Nome Completo', 'trim|required');
+        $this->form_validation->set_rules('apelido', 'Apelido', 'trim|required');
+        $this->form_validation->set_rules('antiguidade', 'Antiguidade', 'trim|required');
+        $this->form_validation->set_rules('nota_curso', 'Nota de Curso', 'trim|required');
+        
+        $postos_bd = $this->militar_model->ler_postos($info);
+        $quarteis_bd = $this->militar_model->ler_quarteis($info);
+        $companhias_bd = $this->militar_model->ler_companhias($info);
+
+        $postos=array();
+        $quarteis=array();
+        $companhias=array();
+
+        foreach($postos_bd as $posto){
+            $postos[$posto['id']] = $posto['posto'];
+            }
+        $postos = array('0' => 'Outra opção') + $postos;
+        foreach($quarteis_bd as $quartel){
+            $quarteis[$quartel['id']] = $quartel['quartel'];
+            }
+        $quarteis = array('0' => 'Outra opção') + $quarteis;
+        foreach($companhias_bd as $companhia){
+            $companhias[$companhia['id']] = $companhia['companhia'];
+            }
+        $companhias = array('0' => 'Outra opção') + $companhias;
+        
         $info['id'] = $id;
-        $emissor = $this->emitter_model->ler($info);
-        $info['emissor'] = $emissor[0];
-        $info['permissoes'] = $this->user_group;
-        $this->template->load('template', 'emitter/view', $info);
+        $militar = $this->militar_model->ler($info);
+        $info['militar'] = $militar[0];
+
+        if ($this->form_validation->run() == true) {
+            
+            unset($info);
+            $info = array();
+            $info = $this->input->post(null, true);
+            unset($info['submit']);
+            //o valor que vem no post, é o do indice. aqui vou buscar o nome do ficheiro
+            //$info['ativo'] = true;
+            $this->militar_model->atualizar($info);
+            
+            //crio os campos que vou usar para fazer o log.
+            $info['user_nim'] = $this->ion_auth->user()->row()->username;
+            $info['tipo'] = 'militares';
+            $info['accao'] = 'editar';
+            $info['informacao'] = 'nim: '.$info['nim'].'; nome: '.$info['nome'].'; apelido: '.$info['apelido'].
+                '; posto: p|'.$info['posto_id'].'; antiguidade: '.$info['antiguidade'].'; nota curso: '.$info['nota_curso'].
+                '; quartel: q|'.$info['quartel_id'].'; companhia: c|'.$info['companhia_id'];
+            $this->registo_model->log_escreve($info);
+
+            redirect('militar/view/'.$info['nim'], 'refresh');
+
+        } else {
+            $info['permissoes'] = $this->user_group;
+            
+            $info['postos'] = $postos;
+            $info['quarteis'] = $quarteis;
+            $info['companhias'] = $companhias;
+
+            $this->template->load('template', 'militar/editar', $info);
+        }
+
     }
 
     /**@
@@ -115,11 +173,7 @@ class Militar extends CI_Controller {
         $this->form_validation->set_rules('apelido', 'Apelido', 'trim|required');
         $this->form_validation->set_rules('antiguidade', 'Antiguidade', 'trim|required');
         $this->form_validation->set_rules('nota_curso', 'Nota de Curso', 'trim|required');
-        //$this->form_validation->set_rules('ativo', 'Descrição', 'trim|required');
-        //$this->form_validation->set_rules('posto_id', 'Descrição', 'trim|required');
-        //$this->form_validation->set_rules('quartel_id', 'Descrição', 'trim|required');
-        //$this->form_validation->set_rules('companhia_id', 'Descrição', 'trim|required');
-
+        
         $info = array();
         $postos_bd = $this->militar_model->ler_postos($info);
         $quarteis_bd = $this->militar_model->ler_quarteis($info);
@@ -141,10 +195,7 @@ class Militar extends CI_Controller {
             $companhias[$companhia['id']] = $companhia['companhia'];
             }
         $companhias = array('0' => 'Outra opção') + $companhias;
-        $info['postos'] = $postos;
-        $info['quarteis'] = $quarteis;
-        $info['companhias'] = $companhias;
-
+        
         if ($this->form_validation->run() == true) {
             
             unset($info);
@@ -167,6 +218,12 @@ class Militar extends CI_Controller {
             redirect('militar', 'refresh');
 
         } else {
+            $info['permissoes'] = $this->user_group;
+            
+            $info['postos'] = $postos;
+            $info['quarteis'] = $quarteis;
+            $info['companhias'] = $companhias;
+
             $this->template->load('template', 'militar/novo', $info);
         }
     }
